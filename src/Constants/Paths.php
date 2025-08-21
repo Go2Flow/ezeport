8ad82c52449964f8507c50200d021602ff243fb2
Jelte ten Holt <jelte.tenholt@go2flow.de>
2025-08-21 09:02:59 +0200

    rebuild project paths

--- src/Constants/Paths.php
+++ src/Constants/Paths.php
@@ -4,28 +4,26 @@ namespace Go2Flow\Ezport\Constants;
 
 class Paths {
 
-    CONST APP_PATH = 'app/Ezport/';
-    CONST PROJECT_PATH = 'Go2Flow/Ezport/';
+    const APP_PATH = 'Ezport/';
+    const PROJECT_PATH = 'Go2Flow/Ezport/';
 
-    public static function appHelpers() : string{
-
-        return self::APP_PATH.'Helpers/';
-    }
-
-    public static function appCustomers() : string {
-
-        return self::APP_PATH . 'Customers/';
-    }
-
-    public static function projectInstructions() : string {
-
-        return self::PROJECT_PATH . 'Instructions/';
-    }
-
-    public static function projectCustomers() : string
+    public static function appHelpers(): string
     {
-
-        return self::PROJECT_PATH . 'Customers/';
+        return app_path(self::APP_PATH.'Helpers/');
     }
 
+    public static function appCustomers(): string
+    {
+        return app_path(self::APP_PATH.'Customers/');
+    }
+
+    public static function projectInstructions(): string
+    {
+        return base_path(self::PROJECT_PATH.'Instructions/');
+    }
+
+    public static function projectCustomers(): string
+    {
+        return base_path(self::PROJECT_PATH.'Customers/');
+    }
 }
