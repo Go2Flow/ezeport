36cdf9d340049b3710047d23dcf2e56eaeb7bd9c
Jelte ten Holt <jelte.tenholt@go2flow.de>
2026-01-27 09:00:45 +0100

    bigger password field

--- database/migrations/2026_01_27_072143_update_connectors_table.php
+++ database/migrations/2026_01_27_072143_update_connectors_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::table('connectors', function (Blueprint $table) {
             $table->boolean('encrypted')->default(false);
+            $table->mediumText('password')->change();
+
         });
     }
 
@@ -23,6 +25,8 @@ return new class extends Migration
     {
         Schema::table('connectors', function (Blueprint $table) {
             $table->dropColumn('encrypted');
+            $table->string('password')->change();
+
         });
     }
 };
